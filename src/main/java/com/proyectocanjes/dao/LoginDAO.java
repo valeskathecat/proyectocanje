5fdaa6d75e18e68610fb7eacb21a26f93f1ba5c9
Valeska <salemthepig@gmail.com>
2016-05-05 21:37:54 -0300

    1

--- src/main/java/com/proyectocanjes/dao/LoginDAO.java
+++
@@ -1,6 +0,0 @@
-package com.proyectocanjes.dao;
-import com.proyectocanjes.model.*;
-
-public interface LoginDAO{    
-       public boolean checkLogin(String userName, String userPassword);
-}
--- src/main/java/com/proyectocanjes/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.proyectocanjes.service;
-import com.proyectocanjes.model.*;
-
-public interface LoginService{    
-       public boolean checkLogin(String userName, String userPassword);
-}
